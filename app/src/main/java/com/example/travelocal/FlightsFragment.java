42e78a6304ed9c9bf359b5083556fe322050c7d0
matiasenad <enadmatias94@gmail.com>
2019-12-12 17:47:17 +0800

    update

---
+++ app/src/main/java/com/example/travelocal/FlightsFragment.java
@@ -0,0 +1,44 @@
+package com.example.travelocal;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FlightsFragment extends Fragment {
+    String[] country = {"New York", "Japan", "Korea", "San Francisco", "Taiwan", "China", "Malaysia", "Singapore"};
+    private View view;
+    private Context context;
+
+    public FlightsFragment(Context ctx){
+        this.context = ctx;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return view = inflater.inflate(R.layout.fragment_flights, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (context, android.R.layout.select_dialog_item, country);
+        AutoCompleteTextView actv  =  view.findViewById(R.id.autoCompleteTextView);
+        AutoCompleteTextView actv1  =  view.findViewById(R.id.autoCompleteTextView2);
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+        actv1.setThreshold(1);
+        actv1.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/travelocal/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/travelocal/ui/main/SectionsPagerAdapter.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.travelocal.FlightsFragment;
 import com.example.travelocal.R;
 
 /**
@@ -17,7 +18,7 @@ import com.example.travelocal.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3, R.string.tab_text_4};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,9 +28,18 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
+        Fragment fragment = new Fragment();
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        switch (position) {
+            case 0:
+                fragment = new FlightsFragment(mContext);
+                break;
+
+        }
+        return fragment;
+
+
     }
 
     @Nullable
@@ -41,6 +51,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return 4;
     }
 }
