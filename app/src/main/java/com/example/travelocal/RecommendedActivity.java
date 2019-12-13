e6aaf79bdad7453f813144b1a8497618a0cbfe2a
matiasenad <enadmatias94@gmail.com>
2019-12-13 14:25:47 +0800

    update

---
+++ app/src/main/java/com/example/travelocal/RecommendedActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecommendedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended);
+    }
+}
